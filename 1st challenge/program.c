553e9ff226841044f56324bbd51c95f3f1d8447f
sakhamuripradeepthi-maker <sakhamuripradeepthi@gmail.com>
2026-09-23 10:32:03 +0530

    Update program.c

--- 1st challenge/program.c
+++ 1st challenge/program.c
@@ -1,1 +1,15 @@
-
+//A student has marks for five subjects.write a c program to read the marks of all five subjects and calulate the total marks,average marks,percentage.display all three results clearly
+#include <stdio.h>
+int main(){
+int s1,s2,s3,s4,s5;
+float total,average,percentage;
+printf("Enter the marks for 5subjects: ");
+scanf("%d %d %d %d %d", &s1,&s2,&s3,&s4,&s5);
+total =s1+s2+s3+s4+s5;
+average =total/5;
+percentage=(total/500)*100;
+printf("Total marks =%f\n",total);
+printf("average marks =%f\n",average);
+printf("percentage =%f\n",percentage);
+return 0;
+}
